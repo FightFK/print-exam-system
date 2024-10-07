4d1b583223ae8eb8883c202b6f99a7dddf835838
FightFK <151952442+FightFK@users.noreply.github.com>
2024-10-07 23:39:53 +0700

    Revert "Merge branch 'master' of https://github.com/FightFK/print-exam-system"
    This reverts commit 30047249479354b611d65f376f87ff316c633aab, reversing
    changes made to 06b94d9f394603777cbd2f6a68499f4beef54079.

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -7,14 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"> {/* ปิดแท็กให้ถูกต้อง */}
-      <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Favicon */}
-        <link rel="icon" href="/images/background.png" type="image/x-icon" />
-      </head>
-
+    <html lang="en">
       <body>
         <div className="flex h-screen">
           {/* SideNav */}
@@ -33,3 +26,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
